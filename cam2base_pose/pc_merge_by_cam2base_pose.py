c4f47f8efdc7298416d9d18b32ee87e02065c094
jude <szzhu@hkclr.hk>
2024-09-27 15:04:25 +0800

    - add code block to trans cloud to specified frame

--- cam2base_pose/pc_merge_by_cam2base_pose.py
+++ cam2base_pose/pc_merge_by_cam2base_pose.py
@@ -23,8 +23,17 @@ def merge_pc(pc_A, pose_A, pc_B, pose_B):
     pc_B_transformed = copy.deepcopy(pc_B).transform(cA_T_cB)
     return pc_A + pc_B_transformed
 
+def rectify_pc(pc_A, pose_A, pc_B, pose_B):
+    Tmatrix_A = matrix_from_pose_vec(pose_A)
+    Tmatrix_B = matrix_from_pose_vec(pose_B)
+
+    cA_T_cB = np.linalg.inv(Tmatrix_A) @ Tmatrix_B
+    pc_B_transformed = copy.deepcopy(pc_B).transform(cA_T_cB)
+    return pc_B_transformed
+
 
 if __name__ == "__main__":
+    rectify = False
     # with open('data.json', 'r') as f:
     root_path = './2024-09-26'
     with open(f'{root_path}/data.json', 'r') as f:
@@ -48,8 +57,16 @@ if __name__ == "__main__":
         print(pc_file_path) 
         print(camera_pose)
         pc_to_be_transformed = o3d.io.read_point_cloud(f'{root_path}/' + pc_file_path)
-        pc_merged = merge_pc(pc_merged, pose_ref, pc_to_be_transformed, camera_pose)
-        o3d.visualization.draw_geometries([pc_merged])
+        if rectify:
+            pc_rectified = rectify_pc(pc_merged, pose_ref, pc_to_be_transformed, camera_pose)
+            o3d.visualization.draw_geometries([pc_rectified])
+            break           
+        else:
+            pc_merged = merge_pc(pc_merged, pose_ref, pc_to_be_transformed, camera_pose)
+            o3d.visualization.draw_geometries([pc_merged])
 
-    o3d.io.write_point_cloud("merged.ply", pc_merged)
+    if rectify:
+        o3d.io.write_point_cloud("rectified.ply", pc_rectified)
+    else:
+        o3d.io.write_point_cloud("merged.ply", pc_merged)
 
